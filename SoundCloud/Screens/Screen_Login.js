98a7d4a4a6becf2f826975bb63f8f1528cd5066c
Davi <hoanghienle69@gmail.com>
2023-11-24 03:17:41 +0700

    Update Screen_Login.js

--- SoundCloud/Screens/Screen_Login.js
+++ SoundCloud/Screens/Screen_Login.js
@@ -35,7 +35,7 @@ export default function Screen_Login({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, backgroundColor: "#fff", alignItems: "center" }}>
       <View
         style={{
           flexDirection: "row",
@@ -228,11 +228,3 @@ export default function Screen_Login({ navigation }) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-  },
-});
